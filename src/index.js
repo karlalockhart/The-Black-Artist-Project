82f767bcadfcd539f54278377c24d16e993eb5df
Karla Lockhart <kblockhart@gmail.com>
2023-11-05 15:06:52 -0800

    working fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => console.log("We are connected!!!"))
+document.addEventListener("DOMContentLoaded", () => fetchData)
+
+function fetchData(){
+    fetch("http://localhost:3000/data")
+    .then(res => res.json)
+    .then(data => console.log(data))
+}
