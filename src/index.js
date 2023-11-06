b2190cb0b21446a275b4e9e349e8f786129f899f
Karla Lockhart <kblockhart@gmail.com>
2023-11-05 19:41:00 -0800

    continuing creating search bar and card elements in index.js & adding css elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", () => fetchData)
 
 function fetchData(){
@@ -5,3 +6,30 @@ function fetchData(){
     .then(res => res.json)
     .then(data => console.log(data))
 }
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('The DOM has Landed!')
+
+    //***SEARCH INPUT***
+const searchInput = document.getElementById("searchInput");
+const results = document.getElementById("results");
+
+//***SEARCH INPUT***
+searchInput.addEventListener("input", () => {
+    const searchTerm = searchInput.value.toLowerCase();
+  //***FETCH DB.JSON & CARD***
+    fetch("http://localhost:3000/data")
+      .then((response) => response.json())
+      .then((data) => {
+        // Filter the cards based on the search term
+        const filteredCards = data.filter((card) => {
+          return card.name.toLowerCase().includes(searchTerm) || card.artist.toLowerCase().includes(searchTerm) || card.category.toLowerCase().includes(searchTerm);;
+        });
+  
+        displayFilteredCards(filteredCards);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  });
+
+})
